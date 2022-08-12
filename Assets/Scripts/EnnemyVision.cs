f840b830b7384cfb6c3b1266e0db2efaf966a05e
exitshadow <exertingshadows@gmail.com>
2022-08-12 20:53:08 +0200

    cameras and layer setup for camo checking + todo

--- Assets/Scripts/EnnemyVision.cs
+++ Assets/Scripts/EnnemyVision.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class EnnemyVision : MonoBehaviour
 {
-    [SerializeField] private Camera view;
+    [SerializeField] private Camera generalView;
+    [SerializeField] private Camera targetedView;
     [SerializeField] private GameObject target;
 
-    private RenderTexture viewRender;
-    private Texture2D currentViewRender;
+    private RenderTexture generalRender;
+    private Texture2D generalRenderTex;
+    private RenderTexture targetRender;
+    private Texture2D targetRenderTex;
     private Vector3 viewTargetPos;
 
     private void Start()
     {
-        viewRender = view.targetTexture;
+        generalRender = generalView.targetTexture;
     }
 
     private void Update()
     {
-        viewTargetPos = view.WorldToViewportPoint(target.transform.position);
+        viewTargetPos = generalView.WorldToViewportPoint(target.transform.position);
         if(     viewTargetPos.x > 0 && viewTargetPos.x < 1
             &&  viewTargetPos.y > 0 && viewTargetPos.y < 1  )
         {
@@ -28,20 +31,24 @@ public class EnnemyVision : MonoBehaviour
 
     private void ReadVision()
     {
-        currentViewRender = new Texture2D(  viewRender.width,
-                                            viewRender.height,
+        generalRenderTex = new Texture2D(  generalRender.width,
+                                            generalRender.height,
                                             TextureFormat.RGB24,
                                             true);
 
-        currentViewRender.ReadPixels(new Rect (0,0, viewRender.width, viewRender.height), 0, 0);
+        generalRenderTex.ReadPixels(new Rect (0,0, generalRender.width, generalRender.height), 0, 0);
 
         // TODO
         // alternatively, as we do have the target position, we can draw a rectangle around
-        // its position to grossly encompass the player's dimensions in ennemy's vision
+        // it to grossly encompass the player's dimensions in ennemy's vision
 
         // the quality of the camouflage is determined by adding all pixels values from
         // this bounding rectangle and average it, then testing it against the averaged result
         // of the texture currently output by the target's OpticCamo Shader.
+
+        // alternatively, we could also find the player sihouette and read this instead,
+        // against all the rest of the bounding rect. this should be more accurate.
+        // use the render layers?
 
         // as these operations are rather expensive, they should happen only when the player is
         // within the ennemy's field of view, not hidden by an object, and not in the Update()
@@ -52,6 +59,15 @@ public class EnnemyVision : MonoBehaviour
 
         // this should be sufficient to emulate the accuracy of an enemy's vision and modulate
         // levels of difficulty.
+
+        // todo list
+        //  fetch render textures for both the general view that renders everything
+        //  and the targeted view that only renders the player
+
+        //  on action, read both pixels from both textures
+        //  create a buffer array for the player's texture as to store only initialized pixels (?)
+        //  average both textures to a single big float
+        //  compare both floats and decide of a camo threshold efficiency
 
     }
 
